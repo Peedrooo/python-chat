5c8f041498a7571d62a7210866233375c95403fc
Pedro Vitor Augusto de Jesus <pedrovitora.jesus@gmail.com>
2023-12-02 20:59:36 -0300

    feat: permite cliente criar server

--- src/models/client.py
+++ src/models/client.py
@@ -1,47 +1,15 @@
 import socket
 import threading
-import json
 import os
+from src.models.server_manager import ServerManager
 
-class Client():
+class Client(ServerManager):
 
     def __init__(self):
         self.client = None
         self.nickname = None
         self.password = None
         self.stop_thread = False
-
-    def enter_server(self):
-        os.system('cls||clear')
-        with open('src/resources/servers.json') as f:
-            data = json.load(f)
-        print('Servers: ', end="")
-        for servers in data:
-            print(servers, end=" ")
-        print('\nServer Name:',end=" ")
-        server_name = input()
-        print('Nickname:', end=" ")
-        self.nickname = input()
-        if self.nickname == 'admin':
-            print("Enter Password for Admin:", end=" ")
-            self.password = input()
-
-        ip = data[server_name]["ip"]
-        port = data[server_name]["port"]
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((ip, port))
-
-    def add_server(self):
-        os.system('cls||clear')
-        server_name = input("Enter a name for the server:")
-        server_ip = input("Enter the ip address of the server:")
-        server_port = int(input("Enter the port number of the server:"))
-
-        with open('src/resources/servers.json', 'r') as f:
-            data = json.load(f)
-        with open('src/resources/servers.json', 'w') as f:
-            data[server_name] = {"ip": server_ip, "port": server_port}
-            json.dump(data, f, indent=4)
 
     def receive(self):
         while True:
@@ -85,7 +53,15 @@ class Client():
                 self.client.send(message.encode('ascii'))
 
     def start(self):
-        self.enter_server()
+        while True:
+            os.system('cls||clear')
+            option = input("(1)Enter server\n(2)Add server\n")
+            if option == '1':
+                self.enter_server()
+                break
+            elif option == '2':
+                self.add_server()
+
         receive_thread = threading.Thread(target=self.receive)
         receive_thread.start()
         write_thread = threading.Thread(target=self.write)
--- src/models/server.py
+++ src/models/server.py
@@ -2,9 +2,9 @@ import socket
 import threading
 from time import sleep
 
-class Server:
+class Server():
 
-    def __init__(self, host = 'localhost', port = 8080, qnt_users = 10):
+    def __init__(self, host = '192.168.1.14', port = 8080, qnt_users = 10):
         self.host = host
         self.port = port
         self.qnt_users = qnt_users
